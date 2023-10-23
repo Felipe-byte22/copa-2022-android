7b95e98543f9a8370ad921710cf8167aa15a1c86
Felipe Cunha <felipescunha21@gmail.com>
2023-10-22 22:35:58 -0300

    Adding the whole app

--- app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
@@ -6,8 +6,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.dio.copa.catar.core.BaseViewModel
+import me.dio.copa.catar.domain.model.Match
 import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.usecase.DisableNotificationUseCase
+import me.dio.copa.catar.domain.usecase.EnableNotificationUseCase
 import me.dio.copa.catar.domain.usecase.GetMatchesUseCase
 import me.dio.copa.catar.remote.NotFoundException
 import me.dio.copa.catar.remote.UnexpectedException
@@ -16,6 +20,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getMatchesUseCase: GetMatchesUseCase,
+    private val disableNotificationUseCase: DisableNotificationUseCase,
+    private val enableNotificationUseCase: EnableNotificationUseCase,
 ) : BaseViewModel<MainUiState, MainUiAction>(MainUiState()) {
 
     init {
@@ -32,11 +38,29 @@ class MainViewModel @Inject constructor(
                     is UnexpectedException ->
                         sendAction(MainUiAction.Unexpected)
                 }
-            }.collect{ matches ->
+            }.collect { matches ->
                 setState {
                     copy(matches = matches)
                 }
             }
+    }
+
+    fun toggleNotification(match: Match) {
+        viewModelScope.launch {
+            runCatching {
+                withContext(Dispatchers.Main) {
+                    val action = if (match.notificationEnabled) {
+                        disableNotificationUseCase(match.id)
+                        MainUiAction.DisableNotification(match)
+                    } else {
+                        enableNotificationUseCase(match.id)
+                        MainUiAction.EnableNotification(match)
+                    }
+
+                    sendAction(action)
+                }
+            }
+        }
     }
 }
 
@@ -44,7 +68,9 @@ data class MainUiState(
     val matches: List<MatchDomain> = emptyList()
 )
 
-sealed class MainUiAction {
-    object Unexpected: MainUiAction()
-    data class MatchesNotFound(val message: String): MainUiAction()
+sealed interface MainUiAction {
+    object Unexpected: MainUiAction
+    data class MatchesNotFound(val message: String) : MainUiAction
+    data class EnableNotification(val match: MatchDomain) : MainUiAction
+    data class DisableNotification(val match: MatchDomain) : MainUiAction
 }
